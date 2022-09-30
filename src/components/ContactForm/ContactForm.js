0d493c0bdb007ccd4f61664f9de294f9e175cbbe
Oleksandr <superbowl@ukr.net>
2022-09-30 12:54:58 +0300

    Adds validation witn Yup

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 import PropTypes from 'prop-types';
+
+let schema = yup.object().shape({
+  name: yup
+    .string()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      'Name is not valid'
+    )
+    .required('This field is required'),
+  number: yup
+    .string()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number is not valid'
+    )
+    .required('This field is required'),
+});
+
+const FormError = ({ name }) => {
+  return (
+    <ErrorMessage
+      name={name}
+      render={message => {
+        return <div>{message}</div>;
+      }}
+    />
+  );
+};
 
 const ContactForm = ({ onSubmit }) => {
   const handleSubmit = (values, { resetForm }) => {
@@ -9,16 +38,20 @@ const ContactForm = ({ onSubmit }) => {
   };
 
   return (
-    <Formik initialValues={{ name: '', number: '' }} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
       <Form>
         <label>
           Name
           <Field
             type="text"
             name="name"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
+            title="Name may contain only letters, apostrophe, dash and spaces without spaces at the beginning and end of the name. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           />
+          <FormError name="name" />
         </label>
         <label>
           Number
@@ -26,8 +59,8 @@ const ContactForm = ({ onSubmit }) => {
             type="tel"
             name="number"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
           />
+          <FormError name="number" />
         </label>
         <button type="submit">Add contact</button>
       </Form>
